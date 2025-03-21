6524d005fe0d5439ba7f2507954031ebb0eb1000
Calden-123 <calden020804@gmail.com>
2025-03-21 13:27:17 +0200

    Deleted all files

--- create_db.py
+++
@@ -1,8 +0,0 @@
-from LostAndFound import create_app
-from LostAndFound.db import db  # Correct import of db
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()  # This creates the tables based on the models
-    print("Tables created successfully!")
